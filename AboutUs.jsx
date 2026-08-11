969905590ed4ab970bce50f54133e3882941f9b1
siddhi1252140022-art <siddhi.1252140022@vit.edu>
2026-08-11 13:36:04 +0530

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <section id="about-us">
+      <h2>About Paradise Nursery</h2>
+
+      <p>
+        Paradise Nursery is an online plant shopping store dedicated to
+        providing a wide variety of beautiful and healthy houseplants.
+      </p>
+
+      <p>
+        Our goal is to make it easy for plant lovers to discover, explore,
+        and purchase plants from the comfort of their home.
+      </p>
+
+      <p>
+        We offer plants from different categories along with useful
+        information such as plant names, prices, and images.
+      </p>
+
+      <p>
+        Paradise Nursery is committed to making plant shopping simple,
+        convenient, and enjoyable for everyone.
+      </p>
+    </section>
+  );
+}
+
+export default AboutUs;
