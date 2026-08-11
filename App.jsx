156d71b6dedf34842daa092be6391f430161dd91
siddhi1252140022-art <siddhi.1252140022@vit.edu>
2026-08-11 19:45:58 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -2,17 +2,24 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  const handleGetStarted = () => {
+    window.location.href = "/plants";
+  };
+
   return (
     <div className="landing-page">
       <div className="landing-content">
         <h1>Paradise Nursery</h1>
 
         <p>
-          Welcome to Paradise Nursery, your destination for beautiful
-          and healthy houseplants.
+          Welcome to Paradise Nursery, your destination for
+          beautiful and healthy houseplants.
         </p>
 
-        <button className="get-started-button">
+        <button
+          className="get-started-button"
+          onClick={handleGetStarted}
+        >
           Get Started
         </button>
       </div>
